fb6e4067a9f5cd8d0bc8dd6c1230027f9bbe5d07
Ganoobpro <tl3130158@gmail.com>
2025-09-15 15:08:10 +0700

    Scanner: Can scan number

--- c_scanner.c
+++ c_scanner.c
@@ -52,6 +52,17 @@ static bool __Scanner_Advance__(Scanner* scanner) {
   return false;
 }
 
+static bool __Scanner_Advance_EOF__(Scanner* scanner, const char* errorMessage) {
+  if (!(__Scanner_Advance__(scanner)))
+  {
+    if (!(scanner->hadError))
+      {__Scanner_Error__(scanner, errorMessage);}
+    return false;
+  }
+
+  return true;
+}
+
 static inline bool
 __Scanner_Is_White_Space__(const char checkChar) {
   return (
@@ -59,6 +70,11 @@ __Scanner_Is_White_Space__(const char checkChar) {
     checkChar == '\t' ||
     checkChar == ' '
   );
+}
+
+static inline bool
+__Scanner_Is_Number__(const char checkChar) {
+  return ('0' <= checkChar && checkChar <= '9');
 }
 
 static void __Scanner_Skip_White_Space__(Scanner* scanner) {
@@ -85,6 +101,9 @@ bool ScannerNext(Scanner* scanner, char* stringInput, const uint64_t length) {
     }
   }
 
+  if (scanner->__current_char == '\n')
+    {return true;}
+
   __Scanner_Error__(scanner, "Input string is out of length!");
   return false;
 }
@@ -100,12 +119,8 @@ bool ScannerNextQuote(Scanner* scanner, char* stringInput, const uint64_t length
 
   stringInput[0] = '"';
   for (size_t charIndex=1; charIndex < length-1; charIndex++) {
-    if (!(__Scanner_Advance__(scanner)))
-    {
-      if (!(scanner->hadError))
-        {__Scanner_Error__(scanner, "Expected \" at the end!");}
-      return false;
-    }
+    if (!(__Scanner_Advance_EOF__(scanner, "Expected \" at the end!")))
+      {return false;}
 
     stringInput[charIndex] = scanner->__current_char;
 
@@ -116,21 +131,73 @@ bool ScannerNextQuote(Scanner* scanner, char* stringInput, const uint64_t length
     }
   }
 
+  if (scanner->__current_char == '\n')
+    {return true;}
+
   __Scanner_Error__(scanner, "Input string is out of length!");
   return false;
 }
 
+int64_t ScannerNextNumber(Scanner* scanner) {
+  int64_t answer = 0, numberSign = 1;
+  __Scanner_Skip_White_Space__(scanner);
+
+  if (scanner->__current_char == '-') {
+    numberSign = -1;
+
+    if (!(__Scanner_Is_Number__(scanner->__current_char)))
+    {
+      __Scanner_Error__(scanner, "Expected number after '-'!");
+      return answer;
+    }
+
+    if (!(__Scanner_Advance_EOF__(scanner, "Expected number after '-'!")))
+      {return answer;}
+  }
+
+  while (
+    __Scanner_Is_Number__(scanner->__current_char) &&
+    (!(__Scanner_Is_White_Space__(scanner->__current_char)))
+  ) {
+    int64_t digit = (int64_t)(scanner->__current_char - '0');
+
+    if (
+      INT64_MIN <= answer * 10 + digit * numberSign &&
+      INT64_MAX >= answer * 10 + digit * numberSign
+    ) {answer = answer * 10 + digit * numberSign;}
+    else
+    {
+      __Scanner_Error__(scanner, "Number input is out of range!");
+      return answer;
+    }
+
+    if (!(__Scanner_Advance__(scanner)))
+      {break;}
+  }
+
+  if (!(
+    scanner->hadError ||
+    __Scanner_Is_Number__(scanner->__current_char)
+  ))
+  {__Scanner_Error__(scanner, "Unknown char in number input!");}
+
+  return answer;
+}
+
 int main() {
-  char inputStr[50];
+  char inputStr[5];
   Scanner testScanner = NewScanner(stdin);
 
   printf("Type: ");
-  ScannerNext(&testScanner, inputStr, 50);
+  ScannerNext(&testScanner, inputStr, 5);
   printf("You've type: %s\n", inputStr);
-  printf("Type string in quotation marks: ");
-  ScannerNextQuote(&testScanner, inputStr, 50);
-  printf("You've type: %s\n", inputStr);
-  ScannerClose(&testScanner);
+  printf("Type a number: ");
+  
+  printf("You've type: %ld\n",
+         ScannerNextNumber(&testScanner));
+
+  if (!(testScanner.hadError))
+    {ScannerClose(&testScanner);}
 
   return 0;
 }
